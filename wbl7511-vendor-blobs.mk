180a6732ad8c826d537c76e6a4140526ecff9a62
DarkWoodens <paulionut2003@gmail.com>
2017-05-12 19:11:22 +0300

    S Plus Vendor Tree

---
+++ wbl7511-vendor-blobs.mk
@@ -0,0 +1,15 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/wbl7511/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/wbl7511/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/wbl7511/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/wbl7511/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/wbl7511/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/wbl7511/thh,system/thh)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/wbl7511/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/wbl7511/vendor,system/vendor)
